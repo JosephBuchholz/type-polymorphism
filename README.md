00fd33c2fbc27aa2bf93b93238574301e6df292a
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2026-04-10 20:06:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Type Theory and Polymorphism
+
+See Milner's paper: https://doi.org/10.1016/0022-0000(78)90014-4
---
+++ milner.rkt
@@ -0,0 +1,169 @@
+#lang racket
+
+;; See Milner's paper: https://doi.org/10.1016/0022-0000(78)90014-4
+
+;; Exp program:
+;; x
+;; (e e)
+;; (if g e d)
+;; (λ (x) e)
+;; (fix (x) e)
+;; (let ([x e]) b)
+
+;; TypedExp program:
+;; [x : t]
+;; [([e : t] [e : t]) : t]
+;; [(if [g : t] [e : t] [d : t]) : t]
+;; [(λ (x : t) [e : t]) : t]
+;; [(fix (x : t) [e : t]) : t]
+;; [(let ([[x : t] [e : t]]) [b : t]) : t]
+
+;; A representation of a prefix
+(struct binding
+  [form  ;; λ, fix, or let
+   type]
+  #:transparent)
+
+;; TypedExp -> Type
+(define (get-type typed-exp)
+  (match typed-exp
+    [`(,x : ,t)
+      t]
+    [_
+      (error 'TODO)]))
+
+;; A Substitution is a (HashOf Type Type)
+
+;; Substitution Type -> Type
+(define (apply-sub S type)
+  (match type
+    [(? symbol? x)
+      (if (hash-has-key? S x)
+        (hash-ref S x)
+        x)]
+    [`(,x -> ,y)
+      `(,(apply-sub S x) ,(apply-sub S y))]))
+
+;; Substitution (HashOf Var Binding) -> (HashOf Var Binding)
+(define (apply-sub/type-env S type-env)
+  (for/hash ([(var bind) type-env])
+    (values var (apply-sub/binding S bind))))
+
+;; Substitution Binding -> Binding
+(define (apply-sub/binding S bind)
+  (define new-type (apply-sub S (binding-type bind)))
+  (binding (binding-form bind) new-type))
+
+;; Substitution TypedExp -> TypedExp
+(define (apply-sub/exp S exp)
+  (match exp
+    [`(,x : ,t)
+      `(,(apply-sub/exp S x) : ,(apply-sub S t))]
+    [(? symbol? x)
+      x]
+    [`(,d ,e)
+      `(,(apply-sub/exp S d) ,(apply-sub/exp S e))]
+    [`(λ (,x : ,t) ,body)
+      `(λ (,x : ,(apply-sub t))
+        ,(apply-sub/exp S body))]
+    [_ (error 'TODO-finish-sub/exp-cases)]))
+
+;; Substitution Substitution -> Substitution
+;; Compute substitution that is equivalent to applying R and then S
+(define (apply-sub/sub S R)
+  (for/hash ([(k v) R])
+    (values k (apply-sub S v))))
+
+;; Basically a gensym maker (makes outputs easier to read).
+(define (make-var-generator [base 't])
+  (define counter 0)
+  (define base-string (symbol->string base))
+  (lambda ([_ base])
+    (define counter-string (number->string counter))
+    (set! counter (+ counter 1))
+    (string->symbol (string-append base-string
+                                   counter-string))))
+
+;; Exp [(HashOf Var Binding)] -> TypedExp
+(define (W-alg f [type-env (hash)] [sym-gen (make-var-generator)])
+  (define (W f type-env)
+    (match f
+      [(? boolean? b)
+        'TODO-boolean]
+      [(? symbol? x)
+        (define res (hash-ref type-env x))
+        (match res
+          [(binding (or 'λ 'fix) type)
+            (define T (hash))
+            (define f^ `(,x : ,type))
+
+            `(,T ,f^)]
+          [(binding 'let type)
+            ;; TODO: wrong type
+            (error 'TODO-let-in-env)
+            (define T (hash))
+            (define f^ `(,x : ,type))
+
+            `(,T ,f^)])]
+      [`(,d ,e)
+        (match-define `(,R ,d^) (W d type-env))
+        (match-define `(,S ,e^) (W e (apply-sub/type-env R type-env)))
+        (define d^-type (get-type d^))
+        (define e^-type (get-type e^))
+        (define new-beta (sym-gen))
+        
+        (define U (U-alg (apply-sub S d^-type) `(,e^-type -> ,new-beta)))
+
+        (define T (apply-sub/sub U (apply-sub/sub S R)))
+        (define new-f `[(,(apply-sub/exp S d^) ,e^) : ,new-beta])
+        (define f^ (apply-sub/exp U new-f))
+
+        `(,T ,f^)]
+      [`(if ,g ,d ,e)
+        'TODO-if]
+      [`(λ (,x) ,d)
+        (define type-beta (sym-gen))
+        (match-define `(,R ,d^) (W d (hash-set type-env x (binding 'λ type-beta))))
+        (define R-beta (apply-sub R type-beta))
+        (define d^-type (get-type d^))
+
+        (define T R)
+        (define f^
+          `[(lambda (,x : ,R-beta)
+            ,d^) : (,R-beta -> ,d^-type)])
+        
+        `(,T ,f^)]
+      [`(fix (,x) ,d)
+        'TODO-fix]
+      [`(let ([,x ,d]) ,e)
+        'TODO-let]))
+
+  (W f type-env))
+
+;; Exp Exp -> Substitution
+;; Robinson's unification algorithm
+(define (U-alg e1 e2)
+  ;; TODO: implement
+  (hash))
+
+;; --- Tests ---
+
+;; cons -> a x b
+#;(define ex-tag-pair1
+  '(λ (a)
+    (λ (b) (λ (c)
+      ((cons
+        ((cons a) b))
+        ((cons a) c))))))
+
+(define ex-identity
+  '(λ (a) a))
+
+(define ex-not
+  '(λ (x)
+    (if x
+        #f
+        #t)))
+
+(define init-env
+  (hash 'x (binding 'λ 'Bool)))
